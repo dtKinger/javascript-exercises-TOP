d8ce00a8fbefdf368b39c6a19c2af3b36805e72d
Daniel King <king.daniel.thomas@gmail.com>
2022-12-16 09:37:29 +0700

    Add boilerplate and active learning examples

---
+++ 02_repeatString/scripts.js
@@ -0,0 +1,39 @@
+/*
+let output = document.querySelector('.output');
+output.innerHTML = '';
+
+let i = 10;
+
+while (i >= 0){
+  const para = document.createElement('input');
+  
+  if (i === 0){
+    para.textContent = `Blast off!!`;
+  } else {
+    para.textContent = `${i} seconds until blast off.`;
+  }
+
+  output.appendChild(para);
+
+  i--
+}
+*/
+
+/* 
+people = ['Chris', 'Anne', 'Colin', 'Terri', 'Phil', 'Lola', 'Sam', 'Kay', 'Bruce'];
+
+// Write a loop that will iterate through the people array.
+for (const person of people){
+  if (person == "Phil" || person == "Lola"){
+    refused.textContent += `${person}, `;
+  } else {
+    admitted.textContent += `${person}, `;
+  }  
+}
+
+refused.textContent =
+refused.textContent.slice(0, (refused.textContent.length - 2)) + ".";
+
+admitted.textContent =
+admitted.textContent.slice(0, (admitted.textContent.length - 2)) + ".";
+*/
