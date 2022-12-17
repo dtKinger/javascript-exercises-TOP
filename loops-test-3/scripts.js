b208237cbf84d0e106194711420d34cf9a95c30f
Daniel King <king.daniel.thomas@gmail.com>
2022-12-17 12:37:01 +0700

    Solves loops-test-3

--- loops-test-3/scripts.js
+++ loops-test-3/scripts.js
@@ -1,21 +1,29 @@
+let x = 500;
+const para = document.createElement('p');
 
-    let i = 500;
-    const para = document.createElement('p');
-
-    function isPrime(num) {
-      for(let i = 2; i < num; i++) {
-        if(num % i === 0) {
-          return false;
-        }
-      }
-
-      return true;
+function isPrime(num) {
+  for(let i = 2; i < num; i++) {
+    if(num % i === 0) {
+      return false;
     }
+  }
+
+  return true;
+}
+
+// Add your code here
+
+do {
+  
+  if (isPrime(x)){
+    para.innerHTML += x + "<br>";
+  }
+
+  x--
+} while (x >= 2 && x <= 500);
 
 
-    // Add your code here
 
-
-    // Don't edit the code below here!
-    const section = document.querySelector('section');
-    section.appendChild(para);
+// Don't edit the code below here!
+const section = document.querySelector('section');
+section.appendChild(para);
