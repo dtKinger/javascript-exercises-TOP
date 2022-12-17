aca446b912c8fed96611deb2065babff56edc672
Daniel King <king.daniel.thomas@gmail.com>
2022-12-17 09:36:43 +0700

    Loop through array and append values to li inside ul

--- loops-test-1/scripts.js
+++ loops-test-1/scripts.js
@@ -7,3 +7,18 @@ const myArray = ['tomatoes', 'chick peas', 'onions', 'rice', 'black beans'];
 
     const section = document.querySelector('section');
     section.appendChild(list);
+
+
+
+// Loop through the array
+
+for (let i = 0; i < myArray.length; i++){
+  // 1. Print items inside a <li></li> and // [createElement('li')]
+  // 2. Give the created element text content
+  // 3. append to the ul 'list' // [appendChild(item)]
+  
+  let item = document.createElement('li');
+  item.textContent = myArray[i];
+  list.appendChild(item);
+
+};
