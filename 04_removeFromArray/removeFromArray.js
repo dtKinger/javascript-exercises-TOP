240c76cb02d8650a7bf86ec3eea1cb18dc639bb1
Daniel King <king.daniel.thomas@gmail.com>
2022-12-18 16:48:15 +0700

    don't remove from array - create an empty one and push every value from array to newArray unless that value is includes in args array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+// The easiest way to get an array of all of the arguments
+// that are passed to a function
+// is using the rest operator. If this is unfamiliar to you look it up!
+const removeFromArray = function(...args) {
+  const array = args[0];
+  const newArray = [];
 
+  array.forEach((item) => {
+    if (!args.includes(item)){
+      newArray.push(item);
+    }
+  });
+    return newArray;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// [1, 2, 3, 4]
