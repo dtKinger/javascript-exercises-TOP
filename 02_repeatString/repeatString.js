00247c96143aabb161f89b6c239cae0da9f62941
Daniel King <king.daniel.thomas@gmail.com>
2022-12-18 11:57:03 +0700

    Pass all tests. Fails when I tried to print the text in addition to the javascript

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,10 +1,15 @@
-let repeatString = function(word, times) {
-  if (times < 0) return 'ERROR';
+const repeatString = function(word, times) {
   let string = '';
-  for (let i = 0; i < times; i++) {
+  if (times < 0) return "ERROR";
+  for (let i = 1; i <= times; i++){
     string += word;
-  }
+  };
   return string;
 };
 
+// let para = document.querySelector('p');
+// para.textContent = string;
+
+
+// Do not edit below this line
 module.exports = repeatString;
