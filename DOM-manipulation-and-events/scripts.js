3835c3e311b48f933720443e9c9798a4646ca002
Daniel King <king.daniel.thomas@gmail.com>
2022-12-20 13:13:52 +0700

    (e) callback and e.target uses

--- DOM-manipulation-and-events/scripts.js
+++ DOM-manipulation-and-events/scripts.js
@@ -28,6 +28,10 @@ Event handlers
 const btn2 = document.querySelector('#btn2');
 btn2.onclick = () => alert("Hello, World!");
 
+btn2.addEventListener('click', function (e) {
+  e.target.classList.toggle('test');
+});
+
 
 const btn3 = document.querySelector('#btn3');
 btn3.addEventListener('click', () => {
