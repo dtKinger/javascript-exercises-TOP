ab0256e216b194dbf719d8cb6e9838d0da91914f
Daniel King <king.daniel.thomas@gmail.com>
2022-12-26 21:21:31 +0700

    my custom fibonacci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  if (n < 0){
+    return "OOPS";
+  }
+  let fibArray = [1, 1];
+  for (let i = 1; i < n; i++){
+    fibArray[fibArray.length] = (fibArray[i] + fibArray[i - 1]);
+ } 
+ return fibArray[n - 1];
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
