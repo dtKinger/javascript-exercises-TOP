f85714cc89a2de5d9c6d084742badec986b03380
Daniel King <king.daniel.thomas@gmail.com>
2022-12-26 18:28:41 +0700

    Use a regular expression to remove punctation, whitespace, and then .toLowerCase() for good measure

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+// Get a string, and remove all
+// punctuation, spaces, and use .toLowerCase()
+  let stripped = string
+                    .replace(/[^\w\s\']|_/g, "")
+                    .replace(/\s+/g, "")
+                    .toLowerCase();
+  let palindromify = Array.from(stripped).reverse().join(""); // stripped => arrayify => reversify => join into palindromify check.
+  // Compare reading the reversed string and the original string. if ===, then true.
+  if (palindromify === stripped) {
+    return true;
+  } else {
+    return false;
+  };
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
