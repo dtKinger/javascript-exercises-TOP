595d6e73c7c1cd32354becefaa03184bc4e290a2
Daniel King <king.daniel.thomas@gmail.com>
2022-12-26 17:54:50 +0700

    sum, subtract, multiply, factorial

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,65 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array){
+  return array.reduce((p, c) => p + c, 0);
 };
 
-const multiply = function() {
 
+/*
+const total = a.reduce((p, c) => {
+    return p + c;
+  }, 0);
+*/
+
+/*
+const totalYears = inventors.reduce((total, inventor) => {
+  return total + totalb;
+}, 0);
+*/
+
+/*
+const multiply = function(array) {
+  return array.length ? array.reduce((accumulator, nextItem) => accumulator * nextItem) : 0;
+};
+*/
+
+
+const multiply = function(array) {
+  return array.reduce((p, c) => p * c, 1)
 };
 
-const power = function() {
-	
+
+const power = function(a, b) {
+	return a ** b;
+};
+// can also use " return Math.pow(a, b); "
+
+/*
+const factorial = function(x) {
+  if (x === 0) return 1;
+  let answer = 1;
+  for (let i = x; i > 0; i--){
+    answer *= i;
+  };
+  return answer;
+};
+*/
+
+// This is another implementation of Factorial that uses recursion
+// THANKS to @ThirtyThreeB!
+const factorial = function(n) {
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n-1);
 };
 
-const factorial = function() {
-	
-};
 
 // Do not edit below this line
 module.exports = {
