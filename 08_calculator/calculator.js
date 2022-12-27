70cb707c9854c1822f8e14468698513c0c2d54c1
Daniel King <king.daniel.thomas@gmail.com>
2022-12-27 13:03:46 +0700

    Re-order calculations

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -10,6 +10,25 @@ const sum = function(array){
   return array.reduce((p, c) => p + c, 0);
 };
 
+const multiply = function(array) {
+  return array.reduce((p, c) => p * c, 1)
+};
+
+
+const power = function(a, b) {
+	return a ** b;
+};
+// can also use " return Math.pow(a, b); "
+
+// This is another implementation of Factorial that uses recursion
+// THANKS to @ThirtyThreeB!
+const factorial = function(n) {
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n-1);
+};
+
 
 /*
 const total = a.reduce((p, c) => {
@@ -29,36 +48,6 @@ const multiply = function(array) {
 };
 */
 
-
-const multiply = function(array) {
-  return array.reduce((p, c) => p * c, 1)
-};
-
-
-const power = function(a, b) {
-	return a ** b;
-};
-// can also use " return Math.pow(a, b); "
-
-/*
-const factorial = function(x) {
-  if (x === 0) return 1;
-  let answer = 1;
-  for (let i = x; i > 0; i--){
-    answer *= i;
-  };
-  return answer;
-};
-*/
-
-// This is another implementation of Factorial that uses recursion
-// THANKS to @ThirtyThreeB!
-const factorial = function(n) {
-  if (n === 0) {
-    return 1;
-  }
-  return n * factorial(n-1);
-};
 
 
 // Do not edit below this line
